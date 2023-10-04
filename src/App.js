f9a9121eb3bfed015cf02c1c7b058ce15cde0108
Po2H <138363358+Po2H@users.noreply.github.com>
2023-10-04 16:07:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
     const makePrediction = async () => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://wela-hnplpcqy3-jow12560.vercel.app/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', },
                 body: JSON.stringify({ categoricalData, numericalData }),
