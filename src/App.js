a0a69b429c52172878745a94decf25672cd4fd11
Po2H <138363358+Po2H@users.noreply.github.com>
2023-10-04 15:53:20 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import './App.css'; // Assuming you will store your CSS in App.css
 
-function App() {
+function BusinessPrediction() {
     const [modalDisplay, setModalDisplay] = useState("none");
     const [categoricalData, setCategoricalData] = useState("");
     const [numericalData, setNumericalData] = useState("");
@@ -9,26 +8,26 @@ function App() {
     const [accuracy, setAccuracy] = useState("");
 
     const options = [
-      'โรงแรม รีสอร์ทและห้องชุด', 'เกสต์เฮ้าส์', 'ที่พักสัมผัสวัฒนธรรมชนบท', 
-      'กิจกรรมที่พักแรมระยะสั้นอื่น ๆ ซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
-      'ลานตั้งค่ายพักแรม ที่จอดรถพ่วงและที่ตั้งที่พักแบบเคลื่อนที่', 
-      'การบริการห้องพักหรือที่พักอาศัยสำหรับนักเรียน/นักศึกษา', 
-      'การบริการที่พักแรมประเภทอื่นๆซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
-      'การบริการด้านอาหารในภัตตาคาร/ร้านอาหาร', 'การบริการด้านอาหารบนแผงลอยและตลาด', 
-      'การบริการด้านอาหารโดยร้านอาหารแบบเคลื่อนที่', 'การบริการด้านการจัดเลี้ยง', 
-      'การบริการอาหารสำหรับกิจการขนส่ง', 'การดำเนินงานของโรงอาหาร', 
-      'การบริการด้านอาหารอื่นๆซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
-      'การบริการด้านเครื่องดื่มที่มีแอลกอฮอล์เป็นหลักในร้าน', 
-      'การบริการด้านเครื่องดื่มที่ไม่มีแอลกอฮอล์เป็นหลักในร้าน', 
-      'การบริการด้านเครื่องดื่มบนแผงลอยและตลาด', 'การบริการด้านเครื่องดื่มโดยร้านเคลื่อนที่'
+        'โรงแรม รีสอร์ทและห้องชุด', 'เกสต์เฮ้าส์', 'ที่พักสัมผัสวัฒนธรรมชนบท', 
+        'กิจกรรมที่พักแรมระยะสั้นอื่น ๆ ซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
+        'ลานตั้งค่ายพักแรม ที่จอดรถพ่วงและที่ตั้งที่พักแบบเคลื่อนที่', 
+        'การบริการห้องพักหรือที่พักอาศัยสำหรับนักเรียน/นักศึกษา', 
+        'การบริการที่พักแรมประเภทอื่นๆซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
+        'การบริการด้านอาหารในภัตตาคาร/ร้านอาหาร', 'การบริการด้านอาหารบนแผงลอยและตลาด', 
+        'การบริการด้านอาหารโดยร้านอาหารแบบเคลื่อนที่', 'การบริการด้านการจัดเลี้ยง', 
+        'การบริการอาหารสำหรับกิจการขนส่ง', 'การดำเนินงานของโรงอาหาร', 
+        'การบริการด้านอาหารอื่นๆซึ่งมิได้จัดประเภทไว้ในที่อื่น', 
+        'การบริการด้านเครื่องดื่มที่มีแอลกอฮอล์เป็นหลักในร้าน', 
+        'การบริการด้านเครื่องดื่มที่ไม่มีแอลกอฮอล์เป็นหลักในร้าน', 
+        'การบริการด้านเครื่องดื่มบนแผงลอยและตลาด', 'การบริการด้านเครื่องดื่มโดยร้านเคลื่อนที่'
     ];
 
     const makePrediction = async () => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('https://wela-hnplpcqy3-jow12560.vercel.app/git', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', },
-                body: JSON.stringify({ categoricalData, numericalData }),
+                body: JSON.stringify({ categorical_data: categoricalData, numerical_data: numericalData }),
             });
 
             const data = await response.json();
@@ -38,6 +37,10 @@ function App() {
         } catch (error) {
             console.error('Error during fetch operation: ', error);
         }
+    };
+
+    const closeModal = () => {
+        setModalDisplay("none");
     };
 
     return (
@@ -72,14 +75,30 @@ function App() {
                         onChange={(e) => setNumericalData(e.target.value)}
                     />
                     <br /><br />
-                    <button type="button" onClick={makePrediction}>
+                    <button id="openModalBtn" type="button" onClick={makePrediction}>
                         Predict
                     </button>
                 </form>
             </div>
 
-            {/* Modal Component can go here, or you can create a separate component file for it */}
+            <div className="modal" style={{ display: modalDisplay }} tabIndex="-1" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h2 className="modal-title">Prediction: {prediction}</h2>
+                        </div>
+                        <div className="modal-body">
+                            <p id="modalBodyText">Accuracy: {accuracy}%</p>
+                            <a id="exampleLink" href="http://example.com">Go to Example.com</a>
+                            <button type="button" className="close" aria-label="Close" onClick={closeModal}>
+                                Done
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-export default App;
+
+export default BusinessPrediction;
